d879c25a2ac55dad16707d8ecb0d5ea5999c9e0e
jaysheel.shah <jaysheel.shah@travelperk.com>
2022-06-01 18:37:57 +0200

    Uploading an image functionality, but has an error

--- app/app/settings.py
+++ app/app/settings.py
@@ -142,3 +142,8 @@ AUTH_USER_MODEL = 'core.User'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+# upload images through the browsable interface
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True,
+}
